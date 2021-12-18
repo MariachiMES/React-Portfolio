fa2fe0b6bade1c80d122a139f95899f86fb060b3
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2021-12-17 19:05:55 -0600

    making web deployment

--- src/components/aboutMe/aboutme.jsx
+++ src/components/aboutMe/aboutme.jsx
@@ -3,7 +3,27 @@ import "./aboutme.scss";
 export default function AboutMe() {
   return (
     <div className="about-me" id="about-me">
-      AboutMe
+      <div className="left">
+        <div className="image-container">
+          <img
+            src="assets/AboutMeCropped.png"
+            alt="Picture of David Ortiz"
+          ></img>
+        </div>
+      </div>
+      <div className="right">
+        <div className="wrapper">
+          <h2>¡Hola!</h2>
+          <h1> my name is david ortiz.</h1>
+          <h3>
+            Web Developer
+            <span></span>
+          </h3>
+          <div className="down-arrow">
+            <i class="fas fa-chevron-down"></i>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
