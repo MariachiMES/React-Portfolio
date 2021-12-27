9097e8199dd948e7cc53e546ba6c7603eab74221
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2021-12-26 18:08:37 -0600

    working li tags with active buttons

--- src/components/aboutMe/aboutme.jsx
+++ src/components/aboutMe/aboutme.jsx
@@ -1,14 +1,21 @@
 import "./aboutme.scss";
+import { init } from "ityped";
+import { useEffect, useRef } from "react";
 
 export default function AboutMe() {
+  const textRef = useRef();
+  useEffect(() => {
+    init(textRef.current, {
+      showCursor: true,
+      backDelay: 1500,
+      strings: [" Web Developer", " Artist", " Musician", " Dad"],
+    });
+  }, []);
   return (
     <div className="about-me" id="about-me">
       <div className="left">
         <div className="image-container">
-          <img
-            src="assets/AboutMeCropped.png"
-            alt="Picture of David Ortiz"
-          ></img>
+          <img src="assets/AboutMeCropped.png" alt="David Ortiz"></img>
         </div>
       </div>
       <div className="right">
@@ -16,7 +23,8 @@ export default function AboutMe() {
           <h2>Hi.</h2>
           <h1> My name is David Ortiz.</h1>
           <h3>
-            Web Developer
+            Professional
+            <span ref={textRef}></span>
             <span></span>
           </h3>
           <div className="down-arrow">
--- src/components/portfolio/portfolio.jsx
+++ src/components/portfolio/portfolio.jsx
@@ -1,9 +1,72 @@
+import { ListItemAvatar } from "@material-ui/core";
+import { getByTitle } from "@testing-library/react";
+import { useEffect, useState } from "react";
 import "./portfolio.scss";
+import PortfolioList from "../portfolioList/portfolioList";
 
 export default function Portfolio() {
+  const [selected, setSelected] = useState("featured");
+  const list = [
+    {
+      id: "featured",
+      title: "Featured",
+    },
+    {
+      id: "web",
+      title: "Web App",
+    },
+    {
+      id: "mobile",
+      title: "Mobile App",
+    },
+    {
+      id: "design",
+      title: "Design",
+    },
+    {
+      id: "content",
+      title: "Content",
+    },
+  ];
   return (
     <div className="portfolio" id="portfolio">
-      Portfolio
+      <h1>Portfolio</h1>
+      <ul>
+        {list.map((item) => (
+          <PortfolioList
+            title={item.title}
+            active={selected === item.id}
+            setSelected={setSelected}
+            id={item.id}
+          />
+        ))}
+      </ul>
+      <div className="container">
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Stuff</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Stuff</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Stuff</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Stuff</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Stuff</h3>
+        </div>
+        <div className="item">
+          <img src="" alt="" />
+          <h3>Stuff</h3>
+        </div>
+      </div>
     </div>
   );
 }
