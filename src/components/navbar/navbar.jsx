9ba9b6c8ef7cffd0456b0e8d39314fe95f3e1b26
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2022-01-02 20:20:20 -0600

    fixes

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -6,7 +6,7 @@ export default function Navbar({ menuOpen, setMenuOpen }) {
       <div className="wrapper">
         <div className="left">
           <a href="porfolio" className="logo">
-            David José Ortiz
+            David Ortiz
           </a>
           <div className="itemContainer">
             <i class="fas fa-phone" id="phone"></i>
