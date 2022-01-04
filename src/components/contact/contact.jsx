a47f8300d73c5c452316af93aa96e20a766805ed
David J Ortiz <87109541+MariachiMES@users.noreply.github.com>
2022-01-03 20:53:42 -0600

    fixed contact form submission

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -3,10 +3,14 @@ import "./contact.scss";
 import { useState } from "react";
 
 export default function Contact() {
-  const [message, setMessage] = useState(false);
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setMessage(true);
+  const FORM_ENDPOINT =
+    "https://public.herotofu.com/v1/83fbaaf0-6d07-11ec-b2d5-af79f33d926c";
+
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
   };
   return (
     <div className="contact" id="contact">
@@ -15,24 +19,19 @@ export default function Contact() {
       </div>
       <div className="right">
         <h2>Contact.</h2>
-        <form
-          form
-          method="POST"
-          action="https://formsubmit.co/davidjortizmusic@gmail.com"
-          enctype="multipart/form-data"
-          onSubmit={handleSubmit}
-        >
+        <form action={FORM_ENDPOINT} onSubmit={handleSubmit} method="POST">
           <label for="email">Email Address:</label>
           <input
             type="text"
-            name="sender email"
+            name="email"
             className="text"
             placeholder="Email"
+            required
           />
           <label for="message">Message:</label>
-          <textarea placeholder="Message" name="message"></textarea>
-          <button type="submit">Send</button>
-          {message && <span>Thanks, I will reply ASAP!</span>}
+          <textarea placeholder="Message" name="message" required></textarea>
+          <button type="submit">Send Message</button>
+          {submitted && <span>Thanks, I will reply ASAP!</span>}
         </form>
       </div>
     </div>
